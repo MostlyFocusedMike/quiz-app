f8361551056c00000b2f24a58a0158a964176f88
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-07-09 12:08:10 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
-  resources :topics, only: [:index, :show, :create]
-  resources :questions, only: [:index, :create]
+  resources :users, except: [:new, :edit]
+  resources :topics, only: [:index, :show]
+  resources :questions, only: [:index]
   resources :scores, only: [:index, :show, :create]
 end
