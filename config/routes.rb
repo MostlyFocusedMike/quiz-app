d94916969d0eb8413557c3fc309ba0246dc6500b
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-07-09 10:13:40 -0400

    make routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :topics, only: [:index, :show, :create]
+  resources :questions, only: [:index, :create]
+  resources :scores, only: [:show, :create]
 end
