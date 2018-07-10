eb8550b5b8698e357df816e578e163ab77d51dd6
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-07-10 16:35:51 -0400

    add id to score object when sent over from topic serializer

--- app/serializers/topic_serializer.rb
+++ app/serializers/topic_serializer.rb
@@ -3,7 +3,7 @@ class TopicSerializer < ActiveModel::Serializer
 
   def scores
     self.object.scores.sort_by{|obj| obj.score}.reverse[0..2].map do |score|
-      {user: score.user.name, score: score.score}
+      {user: score.user.name, score: score.score, id: score.id}
     end
   end
 end
