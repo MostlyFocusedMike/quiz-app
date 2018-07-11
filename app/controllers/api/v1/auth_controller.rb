4e633cbde867ab7900c2f30d1aebb686bae7b873
travisdock <travisdock@gmail.com>
2018-07-11 14:49:01 -0400

    remove byebug

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::AuthController < ApplicationController
 
   def create
-    byebug
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       render json: {username: user.username, id: user.id}, status: 200
