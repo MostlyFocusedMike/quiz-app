4e3a35bd3b901ea379a62b17ef89fbea5a85a1c4
teasacura <teasacuraster@gmail.com>
2018-07-12 09:23:08 -0400

    user scores

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,9 +2,10 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :scores
 
   def scores
-    self.object.scores.map do |score|
+    arr = self.object.scores.map do |score|
       {topic: score.topic.title, score: score.score, id: score.id}
     end
+    arr.sort{ |a,b| a[:score] <=> b[:score] }.reverse[0..2]
   end
 
 end
