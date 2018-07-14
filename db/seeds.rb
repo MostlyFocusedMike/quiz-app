c38cefb08ed307e2574004e4aa547a1933398ea6
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-07-14 09:45:58 -0400

    use looping in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,9 +16,9 @@ tom = User.create(username: "tom", password: "1234")
 angelica = User.create(username: "angelica", password: "1234")
 phil = User.create(username: "phil", password: "1234")
 
-general = Topic.create(title: "General", description: "Stuff and whatnot")
-history = Topic.create(title: "History", description: "It's pretty retro")
-film = Topic.create(title: "Film", description: "Talkie Pictures....they movin'")
+general = Topic.create(title: "General", description: "If you want to get all 20, you'll need to know a little of everything")
+history = Topic.create(title: "History", description: "Events from ancient history to modern pop culture make this category tricky")
+film = Topic.create(title: "Film", description: "If you want to play this topic, ask yourself a question: \"Do I feel lucky?\"")
 
 tom.scores.create(score: 1000, topic: general)
 angelica.scores.create(score: 7900, topic: general)
@@ -30,11 +30,11 @@ phil.scores.create(score: 0, topic: history)
 # q1 = computers.questions.create(question: "q1", correct_answer: "c1", wrong1: "w1", wrong2: "w2", wrong3: "w3")
 # q2 = history.questions.create(question: "q2", correct_answer: "c2", wrong1: "w4", wrong2: "w5", wrong3: "w6")
 
-2.times do
-  data1 = JSON.parse(RestClient.get('https://opentdb.com/api.php?amount=50&category=9&type=multiple'))["results"]
-
-  data1.each do |question|
-    general.questions.create(
+def makeQuestions(data_hash) 
+  data = data_hash[:data]
+  topic = data_hash[:topic]
+  data.each do |question|
+    topic.questions.create(
       question: question["question"],
       correct_answer: question["correct_answer"],
       wrong1: question["incorrect_answers"][0],
@@ -42,32 +42,14 @@ phil.scores.create(score: 0, topic: history)
       wrong3: question["incorrect_answers"][2]
     )
   end
-end
-
+end 
 2.times do
-  data2 = JSON.parse(RestClient.get('https://opentdb.com/api.php?amount=50&category=22&type=multiple'))["results"]
-
-  data2.each do |question|
-    history.questions.create(
-      question: question["question"],
-      correct_answer: question["correct_answer"],
-      wrong1: question["incorrect_answers"][0],
-      wrong2: question["incorrect_answers"][1],
-      wrong3: question["incorrect_answers"][2]
-    )
-  end
-end
-
-2.times do
-  data3 = JSON.parse(RestClient.get('https://opentdb.com/api.php?amount=50&category=11&type=multiple'))["results"]
-
-  data3.each do |question|
-    film.questions.create(
-      question: question["question"],
-      correct_answer: question["correct_answer"],
-      wrong1: question["incorrect_answers"][0],
-      wrong2: question["incorrect_answers"][1],
-      wrong3: question["incorrect_answers"][2]
-    )
-  end
+  data_hash = [
+   {topic: general, data: JSON.parse(RestClient.get('https://opentdb.com/api.php?amount=50&category=9&type=multiple'))["results"]},
+   {topic: history, data: JSON.parse(RestClient.get('https://opentdb.com/api.php?amount=50&category=22&type=multiple'))["results"]},
+   {topic: film, data: JSON.parse(RestClient.get('https://opentdb.com/api.php?amount=50&category=11&type=multiple'))["results"]}
+  ]
+  data_hash.each do |data|
+    makeQuestions(data)
+  end 
 end
